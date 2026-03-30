5b080bbb2e0a5362a759ede0f03d1e0923d7e8f8
CANELA LEDEZMA <49193970@ort>
2026-03-30 09:36:04 -0300

    EJ EXTRA terminado

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.get('/', (req, res) => {
 app.use((req, res) => {
     // Definimos el estado 404 y enviamos el HTML con la imagen del gato
  res.status(404).send(` 404 - Ruta no encontrada. Parece que te perdiste. Acá tenés un gato para ayudarte: 
-       <img src="https://http.cat" alt="Error 404 Cat" width="500"> `);})
+       <img src="https://http.cat/status/404.jpg" alt="Error 404 Cat" width="500"> `);})
 app.listen(port, () => { // Inicio el servidor WEB (escucha
 console.log(`Listening on http://localhost:${port}`)
 })
